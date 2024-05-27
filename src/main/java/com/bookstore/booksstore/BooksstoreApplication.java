fdb6e819928f07f5001ead4e259611ea657d0fd4
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2024-05-27 14:20:12 +0200

    dodat crud za knjige

--- src/main/java/com/bookstore/booksstore/BooksstoreApplication.java
+++ src/main/java/com/bookstore/booksstore/BooksstoreApplication.java
@@ -3,7 +3,6 @@ package com.bookstore.booksstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class BooksstoreApplication {
 	public static void main(String[] args) {
--- src/main/java/com/bookstore/booksstore/controllers/BookController.java
+++ src/main/java/com/bookstore/booksstore/controllers/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,6 +23,32 @@ public class BookController {
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
         return "books";
+    }
+
+    @GetMapping("/books/delete")
+    public String delete(@RequestParam("bookId") Long id) {
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books/new")
+    public String newBook(Model model){
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "book-form";
+    }
+
+    @PostMapping("/books/save")
+    public String saveBook(@ModelAttribute("book") Book book){
+        bookService.saveBook(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books/update")
+    public String updateBook(@RequestParam("bookId") Long id, Model model){
+        Book book = bookService.getBookById(id);
+        model.addAttribute("book", book);
+        return "book-form";
     }
 
 }
