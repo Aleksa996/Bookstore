63396c02ff132f85439b03da0f1067a355e77596
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2024-05-27 14:42:21 +0200

    sredjen update knjige

--- src/main/java/com/bookstore/booksstore/services/BookService.java
+++ src/main/java/com/bookstore/booksstore/services/BookService.java
@@ -1,7 +1,13 @@
 package com.bookstore.booksstore.services;
 
+import com.bookstore.booksstore.entities.Author;
 import com.bookstore.booksstore.entities.Book;
+import com.bookstore.booksstore.entities.Category;
+import com.bookstore.booksstore.entities.Publisher;
+import com.bookstore.booksstore.repositories.AuthorRepository;
 import com.bookstore.booksstore.repositories.BookRepository;
+import com.bookstore.booksstore.repositories.CategoryRepository;
+import com.bookstore.booksstore.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +19,15 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    PublisherRepository publisherRepository;
+
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
     }
@@ -22,6 +37,20 @@ public class BookService {
     }
 
     public Book saveBook(Book book){
+        Author author = book.getAuthor();
+        if(author.getAuthorId() == null){
+            authorRepository.save(author);
+        }
+
+        Category category = book.getCategory();
+        if(category.getCategoryId() == null){
+            categoryRepository.save(category);
+        }
+
+        Publisher publisher = book.getPublisher();
+        if(publisher.getPublisherId() == null){
+            publisherRepository.save(publisher);
+        }
         return bookRepository.save(book);
     }
 
